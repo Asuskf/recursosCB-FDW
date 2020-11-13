e33a35e6a4894dc5e82f7f0b473cda8d7ab97e7e
David <kfarinango@protonmail.com>
2020-11-13 13:06:35 -0500

    Test

--- scrypt.R
+++ scrypt.R
@@ -143,7 +143,8 @@ recursos_CB_SV <- tibble(read_excel("./data/Recursos digitales del curso básico
 recursos_CB <- bind_rows(recursos_CB_EC, recursos_CB_HN, recursos_CB_SV)
 
 
-urls <-to_list(for (val in unlist(recursos_CB['Link al recurso'])) if (is.na(val)) 'No tenemos la URL' else paste("<a href=",val,"target=_blank >","Ir al recurso","</a>"))
+urls <-to_list(for (val in unlist(recursos_CB['Link al recurso'])) if (is.na(val)) 'No tenemos la URL' 
+               else paste("<a href=javascript:window.open('",val,"','','width=600,height=400,left=50,top=50,toolbar=yes');void 0>","Ir al recurso","</a>"))
 
 
 unlist(urls)
